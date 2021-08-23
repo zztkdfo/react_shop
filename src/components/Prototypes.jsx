61ab057d9d110999bf2a78db08735cb6846aa2a1
zztkdfo <zztkdfo@gmail.com>
2021-08-23 11:04:51 +0900

    [react-shop] list 추가, mock 데이터 추가

--- src/components/Prototypes.jsx
+++ src/components/Prototypes.jsx
@@ -1,10 +1,47 @@
 // components/Prototypes.jsx
-
+import {DataList} from './../mock/DataList'
 export default function Prototypes() {
+  const dataList = DataList
   return (
     <main>
       <div className="prototypes">
-        상품 리스트
+        {dataList.map((item) => {
+          const {id, thumbnail, title, price, desc,pieUrl} = item
+          return (
+            <div className="prototype" key={id}>
+              <a href={pieUrl} target="_BLANK" rel="noreferrer">
+                <div
+                  style={{
+                    padding: "25px 0 33px 0",
+                  }}
+                >
+                  <video
+                    autoPlay
+                    loop
+                    playsInline
+                    className="prototype__artwork prototype__edit"
+                    style={{
+                      objectFit: "contain",
+                    }}
+                    src={thumbnail}
+                  />
+                </div>
+              </a>
+
+              <div className="prototype__body">
+                <div className="prototype__title">
+                  <div className="btn btn--primary float--right">
+                    <i className="icon icon--plus" />
+                  </div>
+
+                  {title}
+                </div>
+                <p className="prototype__price">$ {price}</p>
+                <p className="prototype__desc">{desc}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
